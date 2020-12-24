7aa07e28e328a4ab609d439a0edab8a2c8f892c1
pedro <pedrolucca@gmail.com>
2020-12-23 21:46:06 -0300

    feature: consumindo api

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,23 @@
 </template>
 
 <script>
+import api from '../services/api'
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+
+  beforeMount(){
+    api.get('/pokemon')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    })
+  }
 }
 </script>
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseUrl: 'https://pokeapi.co/api/v2/',
+  baseURL: 'https://pokeapi.co/api/v2',
   timeout: 1000,
   headers: {'X-Custom-Header': 'foobar'}
 })
