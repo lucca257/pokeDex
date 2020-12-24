eb780a2af47ea821819794ec408ebfa225e35ffe
pedro <pedrolucca@gmail.com>
2020-12-23 22:40:18 -0300

    feature: busca de pokemon

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,13 @@
 <template>
   <q-page class="flex flex-center">
+    <div class="column items-center">
+      <h2>{{name}}</h2>
+      <q-img :src="url" :ratio="1" width="250px"/>
+    </div>
+    <div class="row justify-around full-width">
+      <q-input filled v-model="search" label="Encontre o seu pokemon" />
+      <q-btn color="purple" label="Pesquisar" @click="getPokemon" />
+    </div>
   </q-page>
 </template>
 
@@ -8,19 +16,33 @@ import api from '../services/api'
 export default {
   name: 'PageIndex',
 
-  beforeMount(){
-    api.get('/pokemon')
-    .then(function (response) {
-      // handle success
-      console.log(response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    })
+  data(){
+    return {
+      name: '',
+      url: '',
+      search: 'ditto'
+    }
+  },
+
+  async beforeMount(){
+    await this.getPokemon()
+  },
+
+  methods: {
+    async getPokemon(){
+      await api.get(`/pokemon/${this.search}`)
+      .then(response => {
+        // handle success
+        const {name, sprites} = response.data
+        console.log(name, sprites)
+        this.name = name
+        this.url = sprites.other.dream_world.front_default
+      })
+      .catch(error => {
+        // handle error
+        console.log(error);
+      })
+    }
   }
 }
 </script>
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: 'https://pokeapi.co/api/v2',
-  timeout: 1000,
+  timeout: 3000,
   headers: {'X-Custom-Header': 'foobar'}
 })
 
